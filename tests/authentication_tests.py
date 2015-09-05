70849ec3739d6dd2b03a74edc36c2908f89cc1d0
yudi5158@gmail.com <yudi5158@gmail.com>
2015-09-05 19:21:04 +0800

    update the test case with right path

--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -4,7 +4,7 @@
 from unittest import TestCase
 from mock import patch
 import sys
-sys.path.append("/Users/diyu/git_project/jenkinsleanring")
+sys.path.append("/Users/diyu/git_project/jenkinslearning")
 import project1.authentication as auth
 
 class StandAloneTests(TestCase):
